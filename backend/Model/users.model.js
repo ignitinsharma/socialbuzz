848d2aa92320e2f1d915c78a630c1dfb6293fb5a
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2023-04-03 21:41:46 +0530

    Create users.model.js

---
+++ backend/Model/users.model.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: false,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+  },
+  { timestamps: true },
+  { versionKey: false }
+);
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = {
+  userModel,
+};
