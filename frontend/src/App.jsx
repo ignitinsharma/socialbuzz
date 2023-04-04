c9be35b3e74f51b1a57bab19a39f8c8942455bc5
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2023-04-04 21:11:46 +0530

    page and structure created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import AllRoutes from "./Components/AllRoutes/AllRoutes";
+
+
+function App() {
+  return (
+    <div className="App">
+      <AllRoutes />
+    </div>
+  );
+}
+
+export default App;
