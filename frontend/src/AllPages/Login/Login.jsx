de74bb385a70f97e77ffb7a91027a41b160d1517
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2023-04-05 14:07:34 +0530

    login page ui done

--- frontend/src/AllPages/Login/Login.jsx
+++ frontend/src/AllPages/Login/Login.jsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
-import { FormControl, FormLabel, Input, Button, Box } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Box,
+  Text,
+  Flex,
+} from "@chakra-ui/react";
+import Loginpng from "../../assets/login.svg";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -13,41 +22,76 @@ const Login = () => {
 
   return (
     <Box>
-      <Box>Navbar</Box>
-      <Box w="30%" mx="auto">
-        <form onSubmit={handleSubmit}>
-          <FormControl isRequired>
-            <FormLabel>Email address</FormLabel>
-            <Input
-              type="email"
-              placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </FormControl>
-
-          <FormControl isRequired mt={4}>
-            <FormLabel>Password</FormLabel>
-            <Input
-              type="password"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </FormControl>
-
-          <Button
-            type="submit"
-            color={"white"}
-            _hover={{ bg: "var(--black-color)" }}
-            bg={"var(--main-color)"}
-            mt={4}
-            isFullWidth
-          >
-            Log In
-          </Button>
-        </form>
+      <Box textAlign={"center"} py="1rem">
+        <Text
+          color="var(--main-color)"
+          cursor={"pointer"}
+          fontSize={{ lg: "2rem", md: "1.3rem", sm: "1rem" }}
+          fontWeight={"bold"}
+        >
+          {" "}
+          &lt;SocialBuzz/&gt;
+        </Text>
       </Box>
+      <Flex
+        mt={{ lg: "2rem", md: "1rem", sm: "1rem" }}
+        justifyContent={"center"}
+        display={{ lg: "flex", md: "block", sm: "block" }}
+        w="90%"
+        gap="1rem"
+        mx="auto"
+      >
+        <Box
+          borderRadius={"8px"}
+          height={"fit-content"}
+          w={{
+            lg: "30%",
+            md: "90%",
+            sm: "90%",
+          }}
+          py={"3rem"}
+          px="2rem"
+          shadow={
+            "rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;"
+          }
+        >
+          <form onSubmit={handleSubmit}>
+            <FormControl isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input
+                type="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </FormControl>
+
+            <FormControl isRequired mt={4}>
+              <FormLabel>Password</FormLabel>
+              <Input
+                type="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </FormControl>
+
+            <Button
+              type="submit"
+              color={"white"}
+              _hover={{ bg: "var(--black-color)" }}
+              bg={"var(--main-color)"}
+              mt={4}
+              isFullWidth
+            >
+              Log In
+            </Button>
+          </form>
+        </Box>
+        <Box mt="2rem">
+          <img src={Loginpng} alt="" />
+        </Box>
+      </Flex>
     </Box>
   );
 };
