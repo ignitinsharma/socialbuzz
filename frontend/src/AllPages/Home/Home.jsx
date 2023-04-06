f5c3083fe8e6807ebbc8482c291b02e421596628
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2023-04-06 12:59:50 +0530

    user section is done

--- frontend/src/AllPages/Home/Home.jsx
+++ frontend/src/AllPages/Home/Home.jsx
@@ -1,11 +1,22 @@
 import { Box } from "@mui/material";
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import UserSection from "./UserSection";
+import { Flex } from "@chakra-ui/react";
 
 const Home = () => {
   return (
     <Box>
       <Navbar />
+      <Flex
+        pt={"5.5rem"}
+        w={"90%"}
+        m="auto"
+        border={"1px solid green"}
+        h="100vh"
+      >
+        <UserSection />
+      </Flex>
     </Box>
   );
 };
---
+++ frontend/src/AllPages/Home/UserSection.jsx
@@ -0,0 +1,60 @@
+import { Box, Divider, Flex, Image, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import WorkIcon from "@mui/icons-material/Work";
+import { useNavigate } from "react-router-dom";
+
+const UserSection = () => {
+  const user = useSelector((store) => store.user);
+  const navigate = useNavigate();
+  //   console.log("user:", user);
+  const [userData, setUserData] = useState(null);
+
+  return (
+    <Box border={"1px solid red"} w={"20%"}>
+      <Flex pr={"10px"} justifyContent={"space-between"}>
+        <Flex columnGap={"5px"}>
+          <Image
+            borderRadius="full"
+            boxSize="50px"
+            src="https://bit.ly/dan-abramov"
+            alt="Dan Abramov"
+          />
+          <Box>
+            <Text
+              cursor={"pointer"}
+              onClick={() => navigate(`/profile/${user._id}`)}
+              fontWeight={"bold"}
+            >
+              Name
+            </Text>
+            <Text fontSize={"12px"}>10 friends</Text>
+          </Box>
+        </Flex>
+        <Box cursor={"pointer"}>
+          <PersonAddIcon />
+        </Box>
+      </Flex>
+      <Divider mt={"0.5rem"} />
+      <Box p="2px">
+        <Flex>
+          <WorkIcon style={{ fontSize: "1rem" }} />
+          <Text ml={"5px"} fontSize={"12px"}>
+            Software engineer
+          </Text>
+        </Flex>
+        <Flex>
+          <LocationOnIcon style={{ fontSize: "1rem" }} />
+          <Text ml={"5px"} fontSize={"12px"}>
+            USA
+          </Text>
+        </Flex>
+      </Box>
+      <Divider mt={"0.5rem"} />
+    </Box>
+  );
+};
+
+export default UserSection;
