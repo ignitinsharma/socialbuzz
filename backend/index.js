580bbfac79eee93ac72e68dad43d302163c972f0
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2023-04-25 15:22:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,22 @@ const cors = require("cors");
 const { userRoute } = require("./Routes/users.route");
 const { postsRoute } = require("./Routes/post.route");
 const { createPost } = require("./Controller/posts");
+/* 
+express.json() middleware parses this string and 
+converts it into a JavaScript object that 
+can be accessed in your application.
+
+like this -> {"name": "John", "age": 30}
+to this -> { name: 'John', age: 30 }
+*/
 server.use(express.json());
 
+/* 
+ By using cors middleware, your Express server will be
+ able to handle requests from other domains, 
+ which can be useful if you're building a client-side web 
+ application that needs to make API requests to your server
+ */
 server.use(
   cors({
     origin: "*",
@@ -28,7 +42,6 @@ server.use("/user", userRoute);
 server.use("/posts", postsRoute);
 
 /* Running server */
-
 server.listen(process.env.port, () => {
   mongoose
     .connect(process.env.mongoURL)
