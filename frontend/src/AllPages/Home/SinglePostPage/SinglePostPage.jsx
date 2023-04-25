8a301c4da5090ebe57280468387f607a3dc9b01b
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2023-04-25 22:28:59 +0530

    Update SinglePostPage.jsx

--- frontend/src/AllPages/Home/SinglePostPage/SinglePostPage.jsx
+++ frontend/src/AllPages/Home/SinglePostPage/SinglePostPage.jsx
@@ -1,24 +1,168 @@
-import { Box } from "@chakra-ui/react";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import Navbar from "../../Navbar/Navbar";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import UserSection from "../UserSection/UserSection";
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Input,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SinglePostPage = () => {
   const { postId } = useParams();
-  console.log("params:", postId);
   const dispatch = useDispatch();
   const { token, user, posts } = useSelector((store) => store);
-  console.log("posts:", posts);
+  const userId = user._id;
+  //   console.log("user:", user);
+  //   console.log("posts:", posts);
   return (
-    <Box>
+    <Box w={"100%"} border={"1px solid red"} m="auto">
       <Box>
         <Navbar />
       </Box>
-      {posts &&
-        posts.map((ele) =>
-          ele._id == postId ? <Box>{console.log(ele, "elel post")}</Box> : null
-        )}
+      <Flex m="auto" w="90%" border={"1px solid black"}>
+        <UserSection />
+        {posts &&
+          posts.map((ele) =>
+            ele._id === postId ? (
+              <Box
+                ml={"3rem"}
+                key={ele._id}
+                mb="10px"
+                p="1rem"
+                border={"1.5px solid #E1E4E8"}
+              >
+                {console.log(ele, "inside ele check")}
+                <Flex>
+                  <Image
+                    borderRadius="full"
+                    boxSize="40px"
+                    src={ele.userPicturePath}
+                    alt="Dan Abramov"
+                    objectFit={"cover"}
+                  />
+                  <Box ml={"10px"}>
+                    <Text
+                      cursor={"pointer"}
+                      _hover={{
+                        textDecoration: "underline",
+                        textDecorationThickness: "0.8px",
+                        textUnderlineOffset: "3px",
+                      }}
+                      _firstLetter={{ textTransform: "capitalize;" }}
+                      fontWeight={"bold"}
+                      //   onClick={() => navigate(`/profile/${ele.userId}`)}
+                    >
+                      {`${ele.firstName} ${ele.lastName}`}
+                    </Text>
+                    <Text
+                      color={"var(--main-color)"}
+                      fontSize={"12px"}
+                      _hover={{ fontSize: "bold" }}
+                    >
+                      #socialbuzz
+                    </Text>
+                  </Box>
+                </Flex>
+                <Box w="80%" m="auto">
+                  <Text
+                    _firstLetter={{ textTransform: "capitalize;" }}
+                    fontSize={"15px"}
+                  >
+                    {ele.description}
+                  </Text>
+                  <Link to={`/post/${ele._id}`}>
+                    <Image
+                      mt="1rem"
+                      w={"100%"}
+                      h="250px"
+                      borderRadius="7px"
+                      _hover={{
+                        boxShadow:
+                          "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;",
+                      }}
+                      src={ele.postPicturePath}
+                      alt={ele.firstName}
+                      objectFit={"cover"}
+                    />
+                  </Link>
+                </Box>
+                {/* Like Dislike button  */}
+                <Box cursor={"pointer"} mt={"1rem"} h={"auto"}>
+                  <span>
+                    {ele.likes.includes(userId) ? (
+                      <FavoriteIcon
+                        // onClick={() => disLikePost(ele._id)}
+                        style={{ color: "red" }}
+                      />
+                    ) : (
+                      <FavoriteBorderIcon />
+                    )}
+                  </span>
+
+                  <span style={{ marginLeft: "0.3rem" }}>
+                    {/* This onclick is toggling the Comment box which is show or hide kind of after click */}
+                    {/* <CommentIcon
+                      onClick={() => setToggleComment(!toggleComment)}
+                    /> */}
+                  </span>
+                  <Box>
+                    <Text fontSize={"15px"}>{ele.likes.length} Likes</Text>
+                    <Box>
+                      {ele.comments?.map((el) => (
+                        <Box
+                          //   py="1rem"
+                          p="0.5rem"
+                          mt={"10px"}
+                          border={"1.5px solid #E1E4E8"}
+                        >
+                          <Flex alignItems={"center"}>
+                            <Image
+                              borderRadius="full"
+                              boxSize="30px"
+                              src={el.user.picturePath}
+                              alt="Dan Abramov"
+                              objectFit={"cover"}
+                            />
+                            <Box ml={"10px"}>
+                              <Text
+                                cursor={"pointer"}
+                                _hover={{
+                                  textDecoration: "underline",
+                                  textDecorationThickness: "0.8px",
+                                  textUnderlineOffset: "3px",
+                                }}
+                                fontSize={"13px"}
+                                _firstLetter={{ textTransform: "capitalize;" }}
+                                fontWeight={"bold"}
+                              >
+                                {`${el.user.firstName} ${el.user.lastName}`}
+                              </Text>
+                              <Text fontSize={"13px"}>{el.userComment}</Text>
+                            </Box>
+                          </Flex>
+                        </Box>
+                      ))}
+                    </Box>
+                    {/* <Link to={`/post/${ele._id}`}>
+                      <Text fontSize={"15px"}>
+                        {ele.comments.length} Comments
+                      </Text>
+                    </Link> */}
+                  </Box>
+                </Box>
+              </Box>
+            ) : null
+          )}
+      </Flex>
     </Box>
   );
 };
